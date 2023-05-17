c197957a125567fd003ee713b7c4bf4c80b112c3
Quentin Marceron <proquentinmarce@gmail.com>
2023-05-17 02:06:25 +0200

    aaa

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,29 @@
 import {Button, Collapse, Stack, Typography} from "@mui/material";
 import {useState} from "react";
 import {ExpandMore} from "@mui/icons-material";
+import homeBackground from '../../assets/img/home.jpg';
 
 export default function App() {
   const [collapse, setCollapse] = useState(true);
 
   return (
     <Collapse in={collapse}>
-      <Stack width={'100vw'} height={'100vh'} justifyContent={'center'} alignItems={'center'} sx={{backgroundColor: '#454545'}}>
+      <Stack
+          width={'100vw'}
+          height={'100vh'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          sx={{backgroundImage: `url(${homeBackground})`, backgroundPosition: 'center', backgroundSize: 'cover'}}
+      >
         <Stack fullWidth height={'100%'} justifyContent={'center'} alignItems={'center'}>
-            <Typography component={'h1'} color={'white'} fontWeight={'bold'} fontSize={70}>Gîte les Auberguets</Typography>
+            <Typography component={'h1'} color={'white'} fontWeight={'bold'} fontSize={100}>Gîte les Auberguets</Typography>
         </Stack>
-        <Button fullWidth sx={{height: '100px', backgroundColor: '#999999'}} variant="contained" onClick={() => setCollapse(false)}>
+        <Button
+            variant="contained"
+            fullWidth
+            sx={{height: '100px', backgroundColor: 'rgba(169, 169, 169, 0.8)'}}
+            onClick={() => setCollapse(false)}
+        >
             <ExpandMore />
         </Button>
       </Stack>
