2c6bacbfeee083cfdb321f493edc9b32ba80b74f
Quentin Marceron <proquentinmarce@gmail.com>
2023-05-21 04:03:06 +0200

    Add alt img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 
-import {
-    useMediaQuery,
-} from "@mui/material";
+// import {
+//     useMediaQuery,
+// } from "@mui/material";
 import Content from "../Content/Content";
 import AuberguetsTheme from "../../assets/muiTheme.js";
 
 export default function App() {
-  const isDesktop = useMediaQuery('(min-width:700px)');
+  // const isDesktop = useMediaQuery('(min-width:700px)');
 
   return (
       <AuberguetsTheme>
